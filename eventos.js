c8f478d72a1ab1e42a231ef3c22752ddae3f16ff
Nauu' s <nahuelalonsouy@gmail.com>
2023-08-07 19:15:29 -0300

    configuracion de alert contenedor div

---
+++ eventos.js
@@ -0,0 +1,10 @@
+function showAlert() {
+    alert("hola!");
+}
+
+const div = document.getElementById('container');
+
+
+div.addEventListener('click', function() {
+    alert("hola! soy el div");
+});
